4e61057c44b2b62263237c79d7e81332471ff41f
Annie Huang <annie.hn@gmail.com>
2020-05-22 22:20:48 +1000

    Create the router middleware

--- server.ts
+++ server.ts
@@ -3,6 +3,15 @@ const port = 5000;
 
 const app = new Application();
 
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get('/api/v1/products', ({ response }: {response: any}) => {
+    response.body = 'Hello World';
+});
+
 console.log(`Server running on port ${port}`);
 
 await app.listen({ port })
