95ce4e9c45bd632881a7b987f299138a92a9bda2
Annie Huang <annie.hn@gmail.com>
2020-05-23 12:49:22 +1000

    Create products.ts that holds the products info and Product interface.

---
+++ controllers/products.ts
@@ -0,0 +1,27 @@
+interface Product {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+}
+
+let products: Product[] = [
+    {
+        id: "1",
+        name: "Product One",
+        description: "This is product one",
+        price: 29.99,
+    },
+    {
+        id: "2",
+        name: "Product Two",
+        description: "This is product two",
+        price: 39.99,
+    },
+    {
+        id: "3",
+        name: "Product Three",
+        description: "This is product three",
+        price: 59.99,
+    },
+];
