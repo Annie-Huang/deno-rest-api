4a2af7e76d8a2df79bb5e914c94eaaf16498b772
Annie Huang <annie.hn@gmail.com>
2020-05-23 15:06:05 +1000

    Create proper addProduct call

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,3 +1,4 @@
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 import { Product } from '../types.ts';
 
 let products: Product[] = [
@@ -33,7 +34,6 @@ const getProducts = ({ response }: {response: any}) => {
 // @desc    Get single product
 // @route   GET /api/v1/products/:id
 const getProduct = ({ params, response }: {params: {id: string}, response: any}) => {
-    response.body = 'get';
     const product: Product | undefined = products.find(p => p.id === params.id);
 
     if(product) {
@@ -53,8 +53,26 @@ const getProduct = ({ params, response }: {params: {id: string}, response: any})
 
 // @desc    Add product
 // @route   Post /api/v1/products
-const addProduct = ({ response }: {response: any}) => {
-    response.body = 'add';
+const addProduct = async ({ request, response }: {request: any, response: any}) => {
+    const body = await request.body();
+
+    if (!request.hasBody) {
+        response.status = 400;
+        response.body = {
+            success: false,
+            msg: 'No data'
+        }
+    } else {
+        const product: Product = body.value
+        product.id = v4.generate();
+        products.push(product);
+
+        response.status = 201;
+        response.body = {
+            success: true,
+            data: product
+        }
+    }
 }
 
 // @desc    Update product
